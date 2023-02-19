cdc1c8b94c2a287bf84a66e26fc3f2556c2c9ed1
Lamon <damianlamonski@gmail.com>
2023-02-19 15:36:27 +0100

    v0.3

--- main.py
+++ main.py
@@ -2,17 +2,28 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-listagosci = open("lista_gosci.txt", "r")
+for i in range(0,2):
 
-img = Image.open("wzor.jpg")
+    imie=input()
 
-I1 = ImageDraw.Draw(img)
-
-font=ImageFont.truetype('StylishCalligraphyDemo-XPZZ.ttf', 200)
-
-while(True):
-    gosc = listagosci.readline()
-    if not gosc:
-        break
-    I1.text((600,2175), gosc, font=font, fill=(255,255,255))
-    img.save(str(gosc)+".jpg")
+    listagosci = open("lista_gosci/lista_gosci_"+imie+".txt", "r")
+    
+    img = Image.open("wzory/wzor_"+imie+".jpg")
+    
+    I1 = ImageDraw.Draw(img)
+    
+    font=ImageFont.truetype('StylishCalligraphyDemo-XPZZ.ttf', 200)
+    
+    while(True):
+        gosc = listagosci.readline()
+        
+        if not gosc:
+            break
+        
+        I1.text((600,2175), gosc, font=font, fill=(255,255,255))
+        
+        img.save("gotowe_zaproszenia_"+imie+"/"+str(gosc)+".jpg")
+    
+    listagosci.close()
+    
+    img.close()
