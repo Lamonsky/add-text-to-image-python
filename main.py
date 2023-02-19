ccb8a066c83497186ee1374cf93fa7b9d740a67d
Lamon <damianlamonski@gmail.com>
2023-02-19 15:11:38 +0100

    v0.2

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ img = Image.open("wzor.jpg")
 
 I1 = ImageDraw.Draw(img)
 
-font=ImageFont.truetype('NotoSansMono-Black.ttf', 65)
+font=ImageFont.truetype('StylishCalligraphyDemo-XPZZ.ttf', 200)
 
 while(True):
     gosc = listagosci.readline()
     if not gosc:
         break
-    I1.text((28,36), gosc, font=font, fill=(0,0,0))
+    I1.text((600,2175), gosc, font=font, fill=(255,255,255))
     img.save(str(gosc)+".jpg")
